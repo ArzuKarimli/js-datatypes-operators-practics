ecc09a6dca6f9ca125dd04ca1930787f139ed695
arzusk <arzusk@code.edu.az>
2024-01-27 11:49:00 -0800

    added task

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+"use strict";
+// let isMarried = true;
+// console.log(isMarried)
+// var surname = "Huseynov";
+// var surname ="test";
+// console.log(surname);
+// //let address="test";
+// let address = "Ehmedli";
+// console.log(address);
+// const age = 27;
+// console.log(age);
+
+// let isMarried = true;
+// isMarried=100;
+// console.log(isMarried);
+//const price= 100;
+// let price = 100;
+// price+=50;
+// console.log(price);
+
+// let stu = {
+//     name:"Semed",
+//     surname:"Huseynov",
+//     age:27
+// };
+// let st2 = {
+//     name:"sajashjsa",
+//     surname:"akajas",
+//     age:27
+// };
+
+// stu=st2;
+// console.log(stu.name + "-" + surname);
+
+//stu = null;
+
+// stu.name="Test";
+
+// console.log(stu.name + "-" + surname)
+
+
+// let name = "XXX";
+// let surname = "YYY";
+
+//let fullName= name+"-"+surname;
+// let fullName = `${name} ${surname}`;
+
+// console.log(fullName);
+
+// let ages = [22,44,55,10,8,"salam"];
+// console.log(ages[ages.length-1]);
+
+// console.log(100+5);
+
+
+// let num1=100;
+// let num2=30;
+
+// let result= num1+num2;
+// console.log(result);
+
+// let num3 = 5**3;
+// console.log(num3);
+
+// let num3 = 5;
+
+// num3++;
+// console.log(num3);
+
+//let num1 = "Salam";
+// let num1 = "55";
+
+// let num2 = 55;
+
+// let result = num2 - num1;
+// console.log(num1 === num2);
+
+// let number = 100;
+ 
+// if (number > 90 && number < 120 ){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// let result = number == 100 ? "yes" : "no";
+// console.log(result);
+
+
+// let weekDay = 3;
+ 
+// switch (weekDay) {
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//        break;
+//     default:
+//         console.log("Not selected");
+//         break;
+// }
+
+// let isMarried = false;
+// if(isMarried){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// let nums = [5,4,3,2,"30"];
+
+// if(nums[nums.length-1]===30){
+//     console.log(nums[nums.length-1])
+// }else{
+//    console.log("not equal");
+// }
+
+// let nums = [1,3,5,6,7,8];
+
+// for (let i = 0; i < nums.length; i++) {
+
+//     var sum = 100;
+//     sum+=i;
+// }
+
+// console.log(sum);
